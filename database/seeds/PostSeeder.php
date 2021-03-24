5416a9fcf0c8852a7462ecd2002ba0b6bbf5296d
crudofederico <crudofederico@gmail.com>
2021-03-24 17:25:36 +0100

    complete

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,22 @@
+<?php
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+use App\Post;
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+         for ($i=0; $i <10 ; $i++) { 
+             $newPost= new Post();
+             $newPost->title=$faker->sentence(4);
+             $newPost->content=$faker->text(500);
+             $newPost->slug=Str::slug($newPost->title);
+             $newPost->save();
+         }   
+    }
+}
